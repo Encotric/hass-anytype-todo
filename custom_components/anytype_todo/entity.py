1c721da19fcb1c127eb31dfe1f31d71d37b7fdba
Sebastian Wilke <sebastian@wilke.id>
2026-04-01 20:18:03 +0000

    Remove device

--- custom_components/anytype_todo/entity.py
+++ custom_components/anytype_todo/entity.py
@@ -2,7 +2,6 @@
 
 from __future__ import annotations
 
-from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .const import ATTRIBUTION
@@ -18,13 +17,3 @@ class AnytypeEntity(CoordinatorEntity[AnytypeDataUpdateCoordinator]):
         """Initialize the shared entity state."""
         super().__init__(coordinator)
         self._attr_unique_id = coordinator.config_entry.entry_id
-        self._attr_device_info = DeviceInfo(
-            identifiers={
-                (
-                    coordinator.config_entry.domain,
-                    coordinator.config_entry.entry_id,
-                ),
-            },
-            name="Anytype ToDo",
-            manufacturer="Encotric / Anytype Association",
-        )
